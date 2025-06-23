f5bc138d1cf44a1149bef367cf9b5b6ce9ce70e7
Gigher <gastelo1301@gmail.com>
2025-06-23 19:12:05 +0300

    22: reviews

--- src/trpc/routers/_app.tsx
+++ src/trpc/routers/_app.tsx
@@ -2,6 +2,7 @@ import { createTRPCRouter } from "../init";
 
 import { authRouter } from "@/modules/auth/server/procedures";
 import { tagsRouter } from "@/modules/tags/server/procedures";
+import { reviewsRouter } from "@/modules/reviews/server/procedures";
 import { libraryRouter } from "@/modules/library/server/procedures";
 import { tenantsRouter } from "@/modules/tenants/server/procedures";
 import { checkoutRouter } from "@/modules/checkout/server/procedures";
@@ -12,6 +13,7 @@ export const appRouter = createTRPCRouter({
   auth: authRouter,
   tags: tagsRouter,
   library: libraryRouter,
+  reviews: reviewsRouter,
   tenants: tenantsRouter,
   checkout: checkoutRouter,
   products: productsRouter,
