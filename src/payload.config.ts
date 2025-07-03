6bdfa271df3d9946e2c24f58242d01bfc915fdb0
Gigher <gastelo1301@gmail.com>
2025-07-03 16:45:11 +0300

    24: access control

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -9,6 +9,8 @@ import sharp from "sharp";
 import { fileURLToPath } from "url";
 import { buildConfig } from "payload";
 
+import { isSuperAdmin } from "./lib/access";
+
 import { Tags } from "./collections/Tags";
 import { Users } from "./collections/Users";
 import { Media } from "./collections/Media";
@@ -58,8 +60,7 @@ export default buildConfig({
       tenantsArrayField: {
         includeDefaultField: false,
       },
-      userHasAccessToAllTenants: (user) =>
-        Boolean(user?.roles?.includes("super-admin")),
+      userHasAccessToAllTenants: (user) => isSuperAdmin(user),
     }),
     // storage-adapter-placeholder
   ],
